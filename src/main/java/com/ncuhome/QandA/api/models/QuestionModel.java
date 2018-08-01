2c34dc5f820c7adad019a61a2268e1ab4065349e
a5467021 <a5467021@163.com>
2018-08-01 20:37:43 +0800

    introduce some safer sql statement solution

--- src/main/java/com/ncuhome/QandA/api/models/QuestionModel.java
+++ src/main/java/com/ncuhome/QandA/api/models/QuestionModel.java
@@ -1,6 +1,7 @@
 package com.ncuhome.QandA.api.models;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -24,13 +25,14 @@ public class QuestionModel {
 	
 	protected String getQuestion(String category) throws SQLException {
 		Connection conn = BaseDatabaseModel.getConn();
-		Statement st = conn.createStatement();
 		
 		String sql = "select description from question "
 				+ "left join name_category on question.cid=name_category.category_id "
-				+ "where name_category.name='" + category + "'";
-		ResultSet rs = st.executeQuery(sql);
+				+ "where name_category.name=?";
+		PreparedStatement psmt = conn.prepareStatement(sql);
+		psmt.setString(1, category);
 		
+		ResultSet rs = psmt.executeQuery();
 		rs.last();
 		int questionAmount = rs.getRow();
 		questionAmount  = utils.randIntRanged(questionAmount) + 1;
